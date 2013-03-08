2f7226e757cbfc3ad48a78071a8b126807da911d
Drew Butler <abutler3@gmail.com>
2013-03-07 21:51:58 -0600

    Pass movie search test

--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,15 @@
 require "open-uri"
 require "json"
+require "ostruct"
 class Api
 
   APIKEY = "cdz6mev7zywr529nreq98fhm"
 
   def self.search_by_title(title)
     url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{URI.encode(title)}&page_limit=1"
-    JSON.parse(open(url).read)
+    full_json = JSON.parse(open(url).read)
+    first_movie = full_json.fetch("movies").first
+    OpenStruct.new(first_movie)
+
   end
 end
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,10 +1,10 @@
 require_relative "../lib/api"
+require "ostruct"
+
 describe Api do
 
   it "should search for movies" do
-    movie_hash = Api.search_by_title("Forrest Gump")
-    movies = movie_hash.fetch("movies")
-    puts movies.inspect
-    movies.first.title.should eq("Forrest Gump")
+    movie = Api.search_by_title("Forrest Gump")
+    movie.title.should eq("Forrest Gump")
   end
 end
