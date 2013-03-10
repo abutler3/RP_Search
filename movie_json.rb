b94ee1ba593533d43e41563f28aa4cbb59a3d8af
Drew Butler <abutler3@gmail.com>
2013-03-10 16:50:28 -0500

    Add front-end to search API

--- movie_json.rb
+++ movie_json.rb
@@ -1,2 +1,28 @@
 require_relative "lib/movie"
 require_relative "lib/api"
+
+
+def find_movie
+  puts "OH HAI. Search?"
+
+  movie_title = gets.chomp
+
+  movie = Api.search_by_title(movie_title)
+
+  puts "Found: #{movie_title}. Score: #{movie.score}"
+
+  puts "Search again?"
+end
+
+find_movie
+
+while true do
+  puts "Search Again (Y/N)"
+  answer = gets.upcase[0]
+  if answer == "Y"
+    find_movie
+  else
+    break
+  end
+end
+
